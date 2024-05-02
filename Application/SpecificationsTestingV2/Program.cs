b4da0c2f04e9913034af2893afaa52a3057d0619
Johnwillliam <mjw-tan@outlook.com>
2024-05-02 19:11:49 +0200

    Cleanup

--- Application/SpecificationsTestingV2/Program.cs
+++ Application/SpecificationsTestingV2/Program.cs
@@ -17,8 +17,6 @@ namespace SpecificationsTestingV2
         {
             try
             {
-                // To customize application configuration such as set high DPI settings or default font,
-                // see https://aka.ms/applicationconfiguration.
                 ApplicationConfiguration.Initialize();
                 new SpecificationsDatabaseModel().Database.EnsureCreated();
                 QuestPDF.Settings.License = LicenseType.Community;
