275008b25cbf3745286f502ede3a84e89dcfe933
Michael Tan <mjw-tan@outlook.com>
2025-03-13 07:49:58 +0100

    Rename solution and projects

--- Application/SpecControle/Properties/Resources.Designer.cs
+++ Application/SpecControle/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SpecificationsTestingV2.Properties {
+namespace SpecControle.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SpecificationsTestingV2.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SpecificationsTestingV2.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SpecControle.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
